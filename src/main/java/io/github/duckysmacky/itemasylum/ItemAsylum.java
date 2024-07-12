77e2046ad3e8ef94ab23e193f0e7327753094e60
DuckySmacky <kriperoka@gmail.com>
2024-07-12 13:52:23 +0300

    initial commit

---
+++ src/main/java/io/github/duckysmacky/itemasylum/ItemAsylum.java
@@ -0,0 +1,11 @@
+package io.github.duckysmacky.itemasylum;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemAsylum extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Item Asylum Plugin is starting up...");
+    }
+}
